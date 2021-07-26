1e0d188ed2109026722dd36cc4e962d38ae90eb0
einchieved <49752360+einchieved@users.noreply.github.com>
2021-07-26 14:49:06 +0200

    added html that can be returned

--- src/webserver.js
+++ src/webserver.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (request, response) => {
     response.send('Hello World');
 });
+
+app.get('/home', (request, response) => {
+    response.sendFile(path.join(__dirname + '/../resources/index.html'))
+})
+
 app.listen(80, () => console.log('Server running on port 80'));
