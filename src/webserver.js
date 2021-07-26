40d3788a8c2e20919b944a92ecb02b5dca8f9ac4
einchieved <49752360+einchieved@users.noreply.github.com>
2021-07-26 13:42:54 +0200

    fixed wrong port displayed in console output

--- src/webserver.js
+++ src/webserver.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/', (request, response) => {
     response.send('Hello World');
 });
-app.listen(80, () => console.log('Server running on port 3000'));
+app.listen(80, () => console.log('Server running on port 80'));
